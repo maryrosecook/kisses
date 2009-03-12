1a47ed4423c8909e5cd6a387790905eaef839403
Mary Cook <maryrosecook@gmail.com>
2009-03-12 12:21:03 +0000

    Made number vagariser

--- lib/maths.rb
+++ lib/maths.rb
@@ -1,20 +1,20 @@
 module Maths
   
-  VAGARIES = {"3" => "thousand", "6" => "million", "9" => "billion", "12" => "trillion"}
+  VAGARIES = {3 => "thousand", 6 => "million", 9 => "billion", 12 => "trillion"}
   
   def self.comma_format(str)
     str ? str.to_s.gsub(/(.)(?=.{3}+$)/, %q(\1,)) : nil
   end
   
   def self.vagarise(num)
+    num = num.to_s
     vaguest = num
    
     if vaguest
-      for vagary in VAGARIES.keys().sort { |x,y| x.to_i <=> y.to_i }
-        if num.to_s.length >= vagary.to_i
-          if num.to_s.length == vagary.to_i
-            vaguest = 1 + " " + VAGARIES[vagary]
-          elsif 
+      for vagary in VAGARIES.keys().sort { |x,y| x <=> y }
+        if num.length >= vagary
+          unit = num[0..2].to_f / (10 ** (3 - (num.length - vagary)))
+          vaguest = unit.round().to_s + " " + VAGARIES[vagary]
         end
       end
     end
